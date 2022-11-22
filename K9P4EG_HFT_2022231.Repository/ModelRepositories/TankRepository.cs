0d9a56050953710aab3844e57727d56908cd1c60
Gergo2108 <gergo082001@stud.uni-obuda.hu>
2022-11-22 10:59:08 +0100

    TankRepository

--- K9P4EG_HFT_2022231.Repository/ModelRepositories/TankRepository.cs
+++ K9P4EG_HFT_2022231.Repository/ModelRepositories/TankRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿
+using K9P4EG_HFT_2022231.Models;
+using K9P4EG_HFT_2022231.Repository.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,25 @@ using System.Threading.Tasks;
 
 namespace K9P4EG_HFT_2022231.Repository
 {
-   public class TankRepository
+   public class TankRepository : GenericRepository<Tank>
     {
+        public TankRepository(TankDbContex ctx) :base(ctx)
+        {
+        }
 
+        public override Tank Read(int id)
+        {
+            return ctx.Tanks.FirstOrDefault(t => t.Id == id);
+        }
+
+        public override void Update(Tank item)
+        {
+            var old = Read(item.Id);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
     }
 }
