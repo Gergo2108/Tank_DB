444d36bf5be2a6ea5befc8f3d609a2e920bc3dcc
Gergo2108 <gergo082001@stud.uni-obuda.hu>
2022-11-22 11:17:28 +0100

    BattleRepository

--- K9P4EG_HFT_2022231.Repository/ModelRepositories/BattleRepository.cs
+++ K9P4EG_HFT_2022231.Repository/ModelRepositories/BattleRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using K9P4EG_HFT_2022231.Models;
+using K9P4EG_HFT_2022231.Repository.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,26 @@ using System.Threading.Tasks;
 
 namespace K9P4EG_HFT_2022231.Repository
 {
-    class BattleRepository
+   public class BattleRepository : GenericRepository<Battle>
     {
+        public BattleRepository(TankDbContex ctx) :base(ctx)
+        {
+
+        }
+
+        public override Battle Read(int id)
+        {
+            return ctx.Battles.FirstOrDefault(b => b.Id == id);
+        }
+
+        public override void Update(Battle item)
+        {
+            var old = Read(item.Id);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
     }
 }
