9c95f56a37c7d0a74ff5830575c95fb5ef3c6f6d
Gergo2108 <gergo082001@stud.uni-obuda.hu>
2022-11-22 11:22:34 +0100

    CountryRepository

--- K9P4EG_HFT_2022231.Repository/ModelRepositories/CountryRepository.cs
+++ K9P4EG_HFT_2022231.Repository/ModelRepositories/CountryRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using K9P4EG_HFT_2022231.Models;
+using K9P4EG_HFT_2022231.Repository.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,25 @@ using System.Threading.Tasks;
 
 namespace K9P4EG_HFT_2022231.Repository
 {
-    class CountryRepository
+    public class CountryRepository : GenericRepository<Country>
     {
+        public CountryRepository(TankDbContex ctx) : base(ctx)
+        {
+                
+        }
+        public override Country Read(int id)
+        {
+            return ctx.Countries.FirstOrDefault(c => c.Id == id);
+        }
+
+        public override void Update(Country item)
+        {
+            var old = Read(item.Id);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
     }
 }
