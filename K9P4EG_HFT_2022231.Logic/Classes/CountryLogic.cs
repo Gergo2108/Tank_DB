2999ba21174f7518db50134e2152cc80a90fb0d1
Gergo2108 <gergo082001@stud.uni-obuda.hu>
2022-11-23 18:14:34 +0100

    CountryLogic

--- K9P4EG_HFT_2022231.Logic/Classes/CountryLogic.cs
+++ K9P4EG_HFT_2022231.Logic/Classes/CountryLogic.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using K9P4EG_HFT_2022231.Logic.Interfaces;
+using K9P4EG_HFT_2022231.Models;
+using K9P4EG_HFT_2022231.Repository.Interface;
 
 namespace K9P4EG_HFT_2022231.Logic
 {
-    class CountryLogic
+    public class CountryLogic : ICountryLogic
     {
+        Repo<Country> repo;
+
+        public CountryLogic(Repo<Country> rep)
+        {
+            this.repo = rep;
+        }
+        public void Create(Country item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            repo.Delete(id);
+        }
+
+        public Country Read(int id)
+        {
+            var country = this.repo.Read(id);
+            if (country == null)
+            {
+                throw new ArgumentException("Tank not exissts");
+            }
+            return country;
+        }
+
+        public IEnumerable<Country> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Country item)
+        {
+            this.repo.Update(item);
+        }
     }
 }
---
+++ K9P4EG_HFT_2022231.Logic/Interfaces/ICountryLogic.cs
@@ -0,0 +1,14 @@
+﻿using K9P4EG_HFT_2022231.Models;
+using System.Collections.Generic;
+
+namespace K9P4EG_HFT_2022231.Logic
+{
+    public interface ICountryLogic
+    {
+        void Create(Country item);
+        void Delete(int id);
+        Country Read(int id);
+        IEnumerable<Country> ReadAll();
+        void Update(Country item);
+    }
+}
