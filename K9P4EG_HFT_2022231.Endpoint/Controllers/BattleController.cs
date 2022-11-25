55d250f41ab25ca41f60ac04abb1c4983646b5ad
Gergo2108 <gergo082001@stud.uni-obuda.hu>
2022-11-25 12:43:30 +0100

    Services

---
+++ K9P4EG_HFT_2022231.Endpoint/Controllers/BattleController.cs
@@ -0,0 +1,58 @@
+﻿using K9P4EG_HFT_2022231.Logic;
+using K9P4EG_HFT_2022231.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace K9P4EG_HFT_2022231.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BattleController : ControllerBase
+    {
+        IBattleLogic BattleLogic;
+
+        public BattleController(IBattleLogic battleLogic)
+        {
+            BattleLogic = battleLogic;
+        }
+
+        // GET: api/<TankController>
+        [HttpGet]
+        public IEnumerable<Battle> ReadAll()
+        {
+            return this.BattleLogic.ReadAll();
+        }
+
+        // GET api/<TankController>/5
+        [HttpGet("{id}")]
+        public Battle Read(int id)
+        {
+            return this.BattleLogic.Read(id);
+        }
+
+        // POST api/<TankController>
+        [HttpPost]
+        public void Create([FromBody] Battle value)
+        {
+            this.BattleLogic.Create(value);
+        }
+        // PUT api/<TankController>/5
+        [HttpPut]
+        public void Update([FromBody] Battle value)
+        {
+            this.BattleLogic.Update(value);
+        }
+
+        // DELETE api/<TankController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.BattleLogic.Delete(id);
+        }
+    }
+}
---
+++ K9P4EG_HFT_2022231/NonCrudService.cs
@@ -0,0 +1,69 @@
+﻿using K9P4EG_HFT_2022231.Logic;
+using K9P4EG_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K9P4EG_HFT_2022231.Client
+{
+      class NonCrudService
+    {
+
+        private RestService rest;
+
+        public NonCrudService(RestService rest)
+        {
+            this.rest = rest;
+        }
+
+        public void AvgWeight()
+        {
+            double item = rest.GetSingle<double>("NonCrud/AvgWeight");
+            Console.WriteLine($"Avarage weight: {item} tonns");
+            Console.ReadLine();
+        }
+
+
+        public void BigestGun()
+        {
+            double item = rest.GetSingle<double>("NonCrud/BigestGun");
+            Console.WriteLine($"Biggest gun size : {item} mm");
+            Console.ReadLine();
+        }
+
+        public void ReadCountryStats()
+        {
+            var items = rest.Get<CountryStatistic>("NonCrud/ReadCountryStats");
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+
+        public void GetTanksByGunRange()
+        {
+            Console.WriteLine("Min=");
+            int min = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Max=");
+            int max = int.Parse(Console.ReadLine());
+
+            var items = rest.Get<Tank>($"NonCrud/GetTanksByGunRange?min={min}&max={max}");
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+
+        public void StrongerCountry()
+        {
+            var item = rest.GetSingle<Country>("NonCrud/StrongerCountry");
+            Console.WriteLine(item);
+            Console.ReadLine();
+        }
+    }
+}
